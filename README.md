4d755936f65da11972353ec77cea1d16e8eb691b
Abuzar <abuzar28102004@gmail.com>
2023-02-21 08:20:52 +0300

    feat: Add aesthetic UI to the game

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 ## In this project i built a simple browser console game.
 
-### Description
-* You play against a computer.
-* You input *Rock*, *Paper* or *Scissors* to the prompt
-* Get the result of current round (game goes 5 rounds)
-* After the final round you get the score and message
+### [PLAY NOW]()
--- app.js
+++ app.js
@@ -1,53 +1,86 @@
+const play = document.querySelector('.play');
+const buttons = document.querySelectorAll('.button');
+const btns = document.querySelector('.buttons');
+
+const texts = document.querySelector('.texts');
+const myScore = document.querySelector('.user-score');
+const pcScore = document.querySelector('.computer-score');
+
+const myChoice = document.querySelector('.my-choice');
+const hisChoice = document.querySelector('.his-choice');
+
+const output = document.querySelector('.output');
+
+const popup = document.querySelector('.popup');
+const playAgain = document.querySelector('.play-again');
+const finalResult = document.querySelector('.final-result');
+
+play.addEventListener('click', () => {
+    play.style.display = 'none';
+    texts.style.display = 'block';
+    buttons.forEach((button) => button.style.display = 'block');
+});
+
+let my = 0;
+let him = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const choice = e.target.getAttribute('data-name');
+        myChoice.textContent = choice;
+        myChoice.style.color = 'green';
+        hisChoice.textContent = getComputerChoice();
+        hisChoice.style.color = 'red';
+        output.textContent = playRound(myChoice.textContent, hisChoice.textContent);
+        pcScore.textContent = him;
+        myScore.textContent = my;
+        if (him === 5 || my === 5) showResult();
+    });
+});
+
 const getComputerChoice = () => {
     const values = ['Rock', 'Paper', 'Scissors']
     const index = Math.floor(Math.random() * values.length);
     return values[index];
 };
 
-
 const playRound = (mine, his) => {
     mine = mine[0].toUpperCase().concat(mine.slice(1).toLowerCase());
     if (mine === 'Rock' && his === 'Paper' || 
     mine === 'Paper' && his === 'Scissors' || 
     mine === 'Scissors' && his === 'Rock') {
+        ++him;
         return `You lose! ${his} ${his === 'Scissors' ? 'beat' : 'beats'} ${mine}`;
     } else if (mine === his) {
         return `No one wins! ${mine} and ${his}`;
     } else if (mine !== 'Rock' && mine !== 'Paper' && mine !== 'Scissors') {
         return 'WRONG VALUE!';
     }
+    ++my;
     return `You win! ${mine} ${mine === 'Scissors' ? 'beat' : 'beats'} ${his}`;
 }
 
-const game = () => {
-    let score = 0;
-    for (let i = 1; i <= 5; i++) {
-        if (i === 5) {
-            console.log('%cFinal round', 'font-size: 25px');
-        } else {
-            console.log(`%cRound number ${i}`, 'font-size: 20px');
-        }
-        const mine = prompt('Enter your choice: ');
-        if (mine === null) {
-            console.log('%cYou stopped the game!', 'font-size: 20px');
-            return 1;
-        }
-        const choice = getComputerChoice();
-        const result = playRound(mine, choice);
-        console.log(result);
-        if (result === 'WRONG VALUE!') {
-            --i;
-            continue;
-        }
-        if (result.includes('You win!')) score++;
-        if (i !== 5) {
-            console.log(`%cyour score is ${score}`,'font-size: 20px');
-        }
+const showResult = () => {
+    btns.style.display = 'none';
+    texts.style.display = 'none';
+    popup.style.display = 'block';
+    if (my == 5) {
+        finalResult.textContent = 'YOU WIN!';
+        finalResult.style.color = 'green';
+    } else if (him === 5) {
+        finalResult.textContent = 'YOU LOSE!';
+        finalResult.style.color = 'red';
     }
-    const machinesScore = 5 - score;
-    const result = score > machinesScore ? `YOU WIN! ${score} : ${machinesScore}`
-    : score === machinesScore ? `IT IS A TIE, ${mine} : ${his}` : `COMPUTER WINS! ${machinesScore} : ${score}`;
-    console.log(`%c${result}`, 'font-size: 35px');
+    my = 0;
+    him = 0;
+    playAgain.addEventListener('click', () => {
+        popup.style.display = 'none';
+        btns.style.display = 'flex';
+        texts.style.display = 'block';
+        myScore.textContent = my;
+        pcScore.textContent = him;
+        myChoice.textContent = '';
+        hisChoice.textContent = '';
+        output.textContent = '';
+    });
 }
-
-game();
